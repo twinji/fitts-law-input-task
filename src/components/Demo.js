3fb7067da2391d08db7cf3c8de6fc481d8f96f78
Vishnu Gunapathi <gunvy005@mymail.unisa.edu.au>
2020-08-22 14:51:36 +0930

    Test basic D3 render

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -1,10 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import * as d3 from 'd3';
 
 export class Demo extends Component {
+
+    state = {
+        circles: [
+            {
+                x: 100,
+                y: 100,
+                radius: 20,
+                color: 'crimson'
+            },
+            {
+                x: 200,
+                y: 200,
+                radius: 20,
+                color: 'blue'
+            }
+        ]
+    }
+
+    componentDidMount() {
+        
+        let svgContainer = d3
+            .select("#demoContainer")
+            .append("svg")
+            .attr("width", 300)
+            .attr("height", 300);
+
+        let circles = svgContainer
+            .selectAll("circle")
+            .data(this.state.circles)
+            .enter()
+            .append("circle");
+
+        let circleAttributes = circles
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y)
+            .attr("r", d => d.radius)
+            .attr("fill", d => d.color)
+                                                    
+
+    }
+
     render() {
         return (
             <div>
                 Demo page
+                <div className="container" id="demoContainer"></div>
             </div>
         )
     }
