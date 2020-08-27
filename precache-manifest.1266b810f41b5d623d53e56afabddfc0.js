b7f20e80973bc53b2cf772655330e3a19d83adfb
Vishnu Gunapathi <gunvy005@mymail.unisa.edu.au>
2020-08-27 14:18:37 +0930

    Updates

---
+++ precache-manifest.1266b810f41b5d623d53e56afabddfc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b6c80c40bcb18f6e7345ca5c18df1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9f015585c4057c0b1b",
+    "url": "/static/css/main.3af2278f.chunk.css"
+  },
+  {
+    "revision": "ac88c25a1387d235fe39",
+    "url": "/static/js/2.159eb6c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.159eb6c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9f015585c4057c0b1b",
+    "url": "/static/js/main.8fe04c31.chunk.js"
+  },
+  {
+    "revision": "d050ddb2c0a2ecc105fd",
+    "url": "/static/js/runtime-main.053b6c09.js"
+  }
+]);
