c9596e438b27310de4bbe4153a717bed7b9d85bc
Vishnu Gunapathi <gunvy005@mymail.unisa.edu.au>
2020-08-27 14:47:23 +0930

    Updates

---
+++ precache-manifest.f5ec4735e90896b8d41562b0f8ab63da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7029ee6370eb8a36388d430e16e52e2d",
+    "url": "/fitts-law-input-task/index.html"
+  },
+  {
+    "revision": "1ad827525613eaf7a7a6",
+    "url": "/fitts-law-input-task/static/css/main.3af2278f.chunk.css"
+  },
+  {
+    "revision": "2294f09828a0714a36bb",
+    "url": "/fitts-law-input-task/static/js/2.54d812b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/fitts-law-input-task/static/js/2.54d812b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad827525613eaf7a7a6",
+    "url": "/fitts-law-input-task/static/js/main.7133ce2c.chunk.js"
+  },
+  {
+    "revision": "e750ab85f3d424cd270b",
+    "url": "/fitts-law-input-task/static/js/runtime-main.f964db4e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08a4326366769150ca421d85cb06f032.js"
+  "/fitts-law-input-task/precache-manifest.f5ec4735e90896b8d41562b0f8ab63da.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fitts-law-input-task/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
